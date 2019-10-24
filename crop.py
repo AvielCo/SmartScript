ac978f7ba70c7ca2ae6768b10e131c9015f07eb0
ranav <ranavital71@gmail.com>
2019-10-24 02:11:01 -0700

    Finished crop functionality of 2 pages scan

--- crop.py
+++ crop.py
@@ -1,24 +1,54 @@
 from cv2 import cv2
+import os
+import logging
+from datetime import datetime
 # import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-def crop(imagePath, coords, saveLocation):
-    img = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
-    x1 = coords["x1"]
-    x2 = coords["x2"]
-    y1 = coords["y1"]
-    y2 = coords["y2"]
-    croppedImage = img[y1:y2, x1:x2]
-    cv2.imwrite(saveLocation, croppedImage)
+
+# Global variables
+
+# Logger
+handlers = [logging.FileHandler('logger.log',mode="w"), logging.StreamHandler()]
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(message)s', datefmt="%H:%M:%S", handlers=handlers)
+
+# Dirs
+projectDir = os.getcwd()
+inputFolder = os.path.join(projectDir, "input")
+outputFolder = os.path.join(projectDir, "output")
+
+# Page details
+
+# Single page dimensions
+cropDimensions = {"x1": 1200, "y1": 400, "x2": 2700, "y2": 2400 }
+# Pages middle margin
+margin = 150
+
+
+def cropSinglePage(imageName: str):
+    img = cv2.imread(os.path.join(inputFolder, imageName), cv2.IMREAD_GRAYSCALE)
+    originalName = imageName
+    x1 = cropDimensions["x1"]
+    x2 = cropDimensions["x2"]
+    y1 = cropDimensions["y1"]
+    y2 = cropDimensions["y2"]
+    for _ in range(2):
+        croppedImage = img[y1:y2, x1:x2]
+        saveLocation = os.path.join(outputFolder, imageName + ".jpg")
+        cv2.imwrite(saveLocation, croppedImage)
+        delta = cropDimensions["x2"] - cropDimensions["x1"]
+        x1 += delta + margin
+        x2 += delta + margin
+        imageName += "2"
+    logging.info("Image " +  originalName + " Cropped successfully.")
+
+def main():
+    imagesInput = os.listdir(inputFolder)
+    for imageName in imagesInput:
+        cropSinglePage(imageName)
 
 if __name__ == "__main__":
-    image = "ran.jpg"
-    cropSize = {"x1": 1200, "y1": 400,"x2": 2700,"y2": 2400 }
-    imagePath = "cropped_images//"
-    imageName = 'ran_cropped'
-    for i in range(2):
-        crop(image, cropSize, imagePath + imageName + ".jpg")
-        delta = cropSize["x2"] - cropSize["x1"]
-        cropSize["x1"] += delta + 150
-        cropSize["x2"] += delta + 150
-        imageName += "2"
+    startTime = datetime.now()
+    logging.info("Script started")
+    main()
+    logging.info("Script ended, execution time: " + str(datetime.now() - startTime))
