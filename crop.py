79ec5cc0ab4c246cfe71cae48826d6bd6e0b2150
ranav <ranavital71@gmail.com>
2019-11-02 21:17:20 -0700

    Added multi threading to the crop function

--- crop.py
+++ crop.py
@@ -2,6 +2,8 @@ from cv2 import cv2
 import os
 import logging
 from datetime import datetime
+import threading
+import concurrent.futures
 # import numpy as np
 # import matplotlib.pyplot as plt
 
@@ -42,13 +44,43 @@ def cropSinglePage(imageName: str):
         imageName += "2"
     logging.info("Image " +  originalName + " Cropped successfully.")
 
-def main():
-    imagesInput = os.listdir(inputFolder)
+def cropFiles(imagesInput):
     for imageName in imagesInput:
         cropSinglePage(imageName)
+
+def runThreads():
+    try:
+        imagesInput = os.listdir(inputFolder)
+    except FileNotFoundError:
+        logging.error("Input file '" + inputFolder + "' not found.")
+        return
+    i = int(len(imagesInput) / 4)
+    chunks = [imagesInput[4 * i : 4 * (i+1)] for i in range(len(imagesInput) // 4 + 1)]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        for i in range(5):
+            executor.submit(cropFiles, chunks[i])
+        # executor.submit(cropFiles, chunks[1])
+        # executor.submit(cropFiles, chunks[2])
+        # executor.submit(cropFiles, chunks[3])
+        # executor.submit(cropFiles, chunks[4])
+    # t1 = threading.Thread(target=cropFiles, args=(chunks[0], ))
+    # t2 = threading.Thread(target=cropFiles, args=(chunks[1], ))
+    # t3 = threading.Thread(target=cropFiles, args=(chunks[2], ))
+    # t4 = threading.Thread(target=cropFiles, args=(chunks[3], ))
+    # t5 = threading.Thread(target=cropFiles, args=(chunks[4], ))
+    # t1.start()
+    # t2.start()
+    # t3.start()
+    # t4.start()
+    # t5.start()
+    # t1.join()
+    # t2.join()
+    # t3.join()
+    # t4.join()
+    # t5.join()
 
 if __name__ == "__main__":
     startTime = datetime.now()
     logging.info("Script started")
-    main()
+    runThreads()
     logging.info("Script ended, execution time: " + str(datetime.now() - startTime))
