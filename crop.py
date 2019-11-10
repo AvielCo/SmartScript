c153df54be0d479b8494b58b3d3b3373a0f4cf4e
ranav <ranavital71@gmail.com>
2019-11-09 22:38:45 -0800

    Added Patches feature

--- crop.py
+++ crop.py
@@ -4,7 +4,7 @@ import logging
 from datetime import datetime
 import threading
 import concurrent.futures
-# import numpy as np
+import numpy as np
 # import matplotlib.pyplot as plt
 
 
@@ -18,7 +18,7 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(
 projectDir = os.getcwd()
 inputFolder = os.path.join(projectDir, "input")
 outputFolder = os.path.join(projectDir, "output")
-patchesFolder = os.path.join(projectDir, "patches_output")
+# outputFolder = os.path.join(projectDir, "patches_output")
 
 # Page details
 
@@ -28,8 +28,7 @@ patchDimensions = {"x": 300, "y": 200, "xOffset": 100, "yOffset": 200//3 }
 # Pages middle margin
 margin = 150
 
-numOfThreads = 5
-
+numOfThreads = 4
 
 def cropSinglePage(imageName: str):
     img = cv2.imread(os.path.join(inputFolder, imageName), cv2.IMREAD_GRAYSCALE)
@@ -41,16 +40,21 @@ def cropSinglePage(imageName: str):
     delta = cropDimensions["x2"] - cropDimensions["x1"]
     for _ in range(2):
         croppedImage = img[y1:y2, x1:x2]
-        saveLocation = os.path.join(outputFolder, imageName + ".jpg")
-        cv2.imwrite(saveLocation, croppedImage)
+        # saveLocation = os.path.join(outputFolder, imageName)
+        cropToPatches(croppedImage, originalName)
+        # cv2.imwrite(saveLocation, croppedImage)
         x1 += delta + margin
         x2 += delta + margin
-        imageName += "2"
+        imageName = imageName[:-4] + "2.jpg"
     logging.info("Image " +  originalName + " Cropped successfully.")
 
 def cropFiles(imagesInput):
     for imageName in imagesInput:
         cropSinglePage(imageName)
+
+def listToChunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
 def runThreads():
     try:
@@ -60,36 +64,38 @@ def runThreads():
         return
     if not os.path.exists(outputFolder):
         os.mkdir(outputFolder)
-    i = int(len(imagesInput) / (numOfThreads - 1))
-    chunks = [imagesInput[(numOfThreads - 1) * i : (numOfThreads - 1) * (i+1)] for i in range(len(imagesInput) // (numOfThreads - 1) + 1)]
+    i = round(len(imagesInput) // (numOfThreads - 1))
+    chunks = list(listToChunks(imagesInput,i))
     with concurrent.futures.ThreadPoolExecutor(max_workers=numOfThreads) as executor:
         for i in range(numOfThreads):
             executor.submit(cropFiles, chunks[i])
 
-# def cropToPatches(imageName: str):
-#     image = cv2.imread(os.path.join(outputFolder, imageName))
-#     x1 = y1 = 0
-#     x2 = xJump = patchDimensions["x"]
-#     y2 = yJump =  patchDimensions["y"]
-#     xOffset = patchDimensions["xOffset"]
-#     yOffset = patchDimensions["yOffset"]
-#     i = 1
-#     while x2 < 1500:
-#         y2 = yJump
-#         while y2 < 2000:
-#             for a in range(3):
-#                 for b in range(3):
-#                     croppedPatch = image[y1 + yOffset * a : y2 + yOffset * a , x1 + xOffset * b : x2 + xOffset * b]
-#                     imageName.replace(".jpg", i + ".jpg")
-#                     saveLocation = os.path.join(patchesFolder, imageName)
-#                     cv2.imwrite(saveLocation, croppedPatch)
-#                     i += 1
-#             y1 += yJump
-#             y2 += yJump
-#         x1 += xJump
-#         x2 += xJump
+def cropToPatches(image, imageName: str):
+    # image = cv2.imread(os.path.join(outputFolder, imageName))
+    x1 = y1 = 0
+    x2 = patchDimensions["x"]
+    y2 = patchDimensions["y"]
+    xOffset = patchDimensions["xOffset"]
+    yOffset = patchDimensions["yOffset"]
+    i = 1
+    imageName = imageName[:-4]
+    while x2 < 1500:
+        j = 0
+        while y2 + yOffset * j < 2000:
+            croppedPatch = image[y1 + yOffset * j : y2 + yOffset * j, x1 : x2]
+            saveLocation = os.path.join(outputFolder, imageName + str(i) + ".jpg")
+            cv2.imwrite(saveLocation, croppedPatch)
+            i += 1
+            j += 1
+        x1 += xOffset
+        x2 += xOffset
 
-
+# def runPatchesThreads():
+#     try:
+#         imagesInput = os.listdir(outputFolder)
+#     except FileNotFoundError:
+#         logging.error("Output file '" + outputFolder + "' not found.")
+#         return
 
 if __name__ == "__main__":
     startTime = datetime.now()
