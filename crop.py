44eb34d7670c19560394155e523e0abc951018d7
ranav <ranavital71@gmail.com>
2019-12-21 17:10:18 -0800

    Added a feature that will crop multiple script types and not only one type

--- crop.py
+++ crop.py
@@ -10,6 +10,8 @@ import numpy as np
 
 # Global variables
 
+numOfImagesCropped = 0
+numOfPatches = 0
 # Logger
 handlers = [logging.FileHandler('logger.log',mode="w"), logging.StreamHandler()]
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(message)s', datefmt="%H:%M:%S", handlers=handlers)
@@ -18,87 +20,103 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(
 projectDir = os.getcwd()
 inputFolder = os.path.join(projectDir, "input")
 outputFolder = os.path.join(projectDir, "output")
-# outputFolder = os.path.join(projectDir, "patches_output")
+
 
 # Page details
 
 # Single page dimensions
-cropDimensions = {"x1": 1200, "y1": 400, "x2": 2700, "y2": 2400 } # 1500 X 2000 px
+
+cropDimensions = {"A1": {"x1": 1200, "y1": 400, "x2": 2700, "y2": 2400, "xDelta": 1500, "yDelta": 2000 }}
 patchDimensions = {"x": 300, "y": 200, "xOffset": 100, "yOffset": 200//3 }
 # Pages middle margin
 margin = 150
 
-numOfThreads = 4
+numOfThreads = 3
 
-def cropSinglePage(imageName: str):
-    img = cv2.imread(os.path.join(inputFolder, imageName), cv2.IMREAD_GRAYSCALE)
+def cropSinglePage(imageName: str, dimensionsDict: dict, folderName):
+    img = cv2.imread(os.path.join(inputFolder, folderName, imageName), cv2.IMREAD_GRAYSCALE)
     originalName = imageName
-    x1 = cropDimensions["x1"]
-    x2 = cropDimensions["x2"]
-    y1 = cropDimensions["y1"]
-    y2 = cropDimensions["y2"]
-    delta = cropDimensions["x2"] - cropDimensions["x1"]
+    x1 = dimensionsDict["x1"]
+    x2 = dimensionsDict["x2"]
+    y1 = dimensionsDict["y1"]
+    y2 = dimensionsDict["y2"]
+    delta = dimensionsDict["x2"] - dimensionsDict["x1"]
     for _ in range(2):
         croppedImage = img[y1:y2, x1:x2]
         # saveLocation = os.path.join(outputFolder, imageName)
-        cropToPatches(croppedImage, originalName)
+        saveName = imageName[:-4] #Get the name of the image without the extension (e.g. without '.jpg')
+        cropToPatches(croppedImage, saveName, dimensionsDict["xDelta"], dimensionsDict["yDelta"], folderName)
         # cv2.imwrite(saveLocation, croppedImage)
         x1 += delta + margin
         x2 += delta + margin
         imageName = imageName[:-4] + "2.jpg"
+    global numOfImagesCropped
+    numOfImagesCropped += 1
     logging.info("Image " +  originalName + " Cropped successfully.")
 
-def cropFiles(imagesInput):
+def cropFiles(imagesInput, dimensionsDict, folderName):
     for imageName in imagesInput:
-        cropSinglePage(imageName)
+        cropSinglePage(imageName, dimensionsDict, folderName)
 
 def listToChunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-def runThreads():
+def runThreads(folderName, dimensionsDict):
+    path = os.path.join(inputFolder, folderName)
     try:
-        imagesInput = os.listdir(inputFolder)
+        imagesInput = os.listdir(path)
     except FileNotFoundError:
-        logging.error("Input file '" + inputFolder + "' not found.")
+        logging.error("Input file '" + path + "' not found.")
         return
     if not os.path.exists(outputFolder):
         os.mkdir(outputFolder)
+    if not os.path.exists(os.path.join(outputFolder, folderName)):
+        os.mkdir(os.path.join(outputFolder, folderName))
     i = round(len(imagesInput) // (numOfThreads - 1))
     chunks = list(listToChunks(imagesInput,i))
     with concurrent.futures.ThreadPoolExecutor(max_workers=numOfThreads) as executor:
         for i in range(numOfThreads):
-            executor.submit(cropFiles, chunks[i])
+            executor.submit(cropFiles, chunks[i], dimensionsDict, folderName)
 
-def cropToPatches(image, imageName: str):
+def cropToPatches(image, imageName: str, xDelta, yDelta, folderName):
     # image = cv2.imread(os.path.join(outputFolder, imageName))
     x1 = y1 = 0
-    x2 = patchDimensions["x"]
-    y2 = patchDimensions["y"]
-    xOffset = patchDimensions["xOffset"]
-    yOffset = patchDimensions["yOffset"]
+    x2 , y2 = patchDimensions["x"], patchDimensions["y"]
+    xOffset, yOffset = patchDimensions["xOffset"], patchDimensions["yOffset"]
     i = 1
-    imageName = imageName[:-4]
-    while x2 < 1500:
+    while x2 < xDelta:
         j = 0
-        while y2 + yOffset * j < 2000:
+        while y2 + yOffset * j < yDelta:
             croppedPatch = image[y1 + yOffset * j : y2 + yOffset * j, x1 : x2]
-            saveLocation = os.path.join(outputFolder, imageName + str(i) + ".jpg")
+            saveLocation = os.path.join(outputFolder, folderName, imageName + str(i) + ".jpg")
+            global numOfPatches
+            numOfPatches += 1
             cv2.imwrite(saveLocation, croppedPatch)
             i += 1
             j += 1
         x1 += xOffset
         x2 += xOffset
+    
+def preProcessingMain():
+    try:
+        foldersName = os.listdir(inputFolder)
+    except FileNotFoundError:
+        logging.error("Input file '" + inputFolder + "' not found.")
+        return
+    for name in foldersName:
+        logging.info("Start cropping the folder " + name + ".")
+        try:
+            runThreads(name, cropDimensions[name])
+            logging.info("Cropping of " + name + " succeeded.")
+        except KeyError:
+            logging.error("File name doesn't match to the dictionary's key.")
+            logging.error("Cropping of " + name + " Failed.")
 
-# def runPatchesThreads():
-#     try:
-#         imagesInput = os.listdir(outputFolder)
-#     except FileNotFoundError:
-#         logging.error("Output file '" + outputFolder + "' not found.")
-#         return
 
 if __name__ == "__main__":
     startTime = datetime.now()
     logging.info("Script started")
-    runThreads()
+    preProcessingMain()
     logging.info("Script ended, execution time: " + str(datetime.now() - startTime))
+    logging.info(str(numOfImagesCropped) + " Images have been cropped into " + str(numOfPatches) + " Patches.")
