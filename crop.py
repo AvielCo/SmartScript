c4ad042c285f4aaf31913c025fda1140480885bc
Aviel Cohen <38301471+AvielCo@users.noreply.github.com>
2020-09-03 12:59:37 +0300

    Cropped patches correctly

--- crop.py
+++ crop.py
@@ -4,23 +4,26 @@ import inspect
 from pytz import timezone
 import concurrent.futures
 from datetime import datetime, timedelta
-from cv2 import cv2
+import cv2
 import subprocess
+import matplotlib.pyplot as plt
+import numpy as np
+
+from PIL import Image
 
 # Global variables
-NUM_OF_CROPPED_IMAGES=0
-NUM_OF_PATCHES=0
+NUM_OF_CROPPED_IMAGES = 0
+NUM_OF_PATCHES = 0
 # Threads
-NUM_OF_THREADS=2
+NUM_OF_THREADS = 2
 
 # Logger
 tz = timezone('Asia/Jerusalem')
-handlers = [logging.FileHandler('logs/' + str(datetime.now(tz).strftime("%d-%m_%H:%M:%S")) + '_logger.log', mode="w"), logging.StreamHandler()]
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(message)s', datefmt="%d/%m/%y - %H:%M:%S",
-                    handlers=handlers)
+
 
 def timetz(*args):
     return datetime.now(tz).timetuple()
+
 
 logging.Formatter.converter = timetz
 
@@ -28,41 +31,27 @@ logging.Formatter.converter = timetz
 PROJECT_DIR = os.getcwd()
 INPUT_PATH = os.path.join(PROJECT_DIR, "input")
 OUTPUT_PATH = os.path.join(PROJECT_DIR, "output")
+UNFILTERED_PATCHES_PATH = os.path.join(PROJECT_DIR, "unfiltered_patches")
+CURSIVE = "cursive"
+SEMI_SQUARE = "semi_square"
+SQUARE = "square"
+CURSIVE_INPUT_PATH = os.path.join(INPUT_PATH, CURSIVE)
+SEMI_SQUARE_INPUT_PATH = os.path.join(INPUT_PATH, SEMI_SQUARE)
+SQUARE_INPUT_PATH = os.path.join(INPUT_PATH, SQUARE)
+CURSIVE_UNFILTERED_PATH = os.path.join(UNFILTERED_PATCHES_PATH, CURSIVE)
+SEMI_SQUARE_UNFILTERED_PATH = os.path.join(UNFILTERED_PATCHES_PATH, SEMI_SQUARE)
+SQUARE_UNFILTERED_PATH = os.path.join(UNFILTERED_PATCHES_PATH, SQUARE)
+CURSIVE_OUTPUT_PATH = os.path.join(OUTPUT_PATH, CURSIVE)
+SEMI_SQUARE_OUTPUT_PATH = os.path.join(OUTPUT_PATH, SEMI_SQUARE)
+SQUARE_OUTPUT_PATH = os.path.join(OUTPUT_PATH, SQUARE)
 
 # Page details
 # Single page dimensions
-CROP_DIMENSIONS = {"A1": {"x1": 1000, "y1": 350, "x2": 2750, "y2": 2800, "margin": 200, "pageNum": 2},
-                  "A2": {"x1": 1000, "y1": 550, "x2": 3300, "y2": 3650, "margin": 800, "pageNum": 2},
-                  "A3": {"x1": 1000, "y1": 700, "x2": 3200, "y2":3750, "margin": 700, "pageNum": 2},
-                  "A4": {"x1": 2450, "y1": 1200, "x2": 5200, "y2":5900, "margin": 1000, "pageNum": 2},
-                  "B1": {"x1": 700, "y1": 400, "x2": 2800, "y2": 3600, "margin": 300, "pageNum": 2},
-                  "B2": {"x1": 180, "y1": 130, "x2": 1600, "y2": 2200, "margin": 330, "pageNum": 2},
-                  "B3": {"x1": 950, "y1": 400, "x2": 2900, "y2": 2850, "margin": 350, "pageNum": 2}, 
-                  "I": {"x1": 750, "y1": 700, "x2": 3200, "y2": 4500, "margin": 0, "pageNum": 1},
-                  "S": {"x1": 1250, "y1": 925, "x2": 3025, "y2":3700, "margin": 400, "pageNum": 2}
-                  }
 
-PATCH_DIMENSIONS = {"x": 300, "y": 200, "xOffset": 300 // 3, "yOffset": 200 // 3}
+PATCH_DIMENSIONS = {"x": 500, "y": 500, "xOffset": 500 // 2, "yOffset": 500 // 2}
 
 
-def getMinResolution():
-    """
-    This function returns the minimum resolution of the cropDimesions dict.
-
-    Returns:
-    dict: The minimum height and width.
-    """
-    height = width = 200000
-    for key in CROP_DIMENSIONS.keys():
-        if CROP_DIMENSIONS[key]["x2"] - CROP_DIMENSIONS[key]["x1"] < width:
-            width = CROP_DIMENSIONS[key]["x2"] - CROP_DIMENSIONS[key]["x1"]
-        if CROP_DIMENSIONS[key]["y2"] - CROP_DIMENSIONS[key]["y1"] < height:
-            height = CROP_DIMENSIONS[key]["y2"] - CROP_DIMENSIONS[key]["y1"]
-    return {"height": height, "width": width}
-
-RESIZE_UNITS = getMinResolution()
-
-def cropSinglePage(imageName: str, dimensionsDict: dict, folderName: str):
+def cropSinglePage(imageName: str, folderName: str):
     """
     This function crops a single page from the scan (by its dimensions).
 
@@ -71,27 +60,25 @@ def cropSinglePage(imageName: str, dimensionsDict: dict, folderName: str):
     dimensionsDict (dict): The dimensions of the scanned image.
     folderName (str): The name of the folder that the scan is saved in.
     """
-    img = cv2.imread(os.path.join(INPUT_PATH, folderName, imageName), cv2.IMREAD_GRAYSCALE) # Read the image from the folder with grayscale mode
-    originalName = imageName # For the log
-    x1 = dimensionsDict["x1"]
-    x2 = dimensionsDict["x2"]
-    y1 = dimensionsDict["y1"]
-    y2 = dimensionsDict["y2"]
-    delta = dimensionsDict["x2"] - dimensionsDict["x1"] # The offset to move to the next page of the scan
-    for _ in range(dimensionsDict["pageNum"]):
-        croppedImage = img[y1:y2, x1:x2] # Crop the margins of the image
-        croppedImage = cv2.resize(croppedImage, (RESIZE_UNITS["width"], RESIZE_UNITS["height"])) # Resize the image to work with the same size of the manuscript
-        saveName = os.path.splitext(imageName)[0]  # Get the name of the image without the extension (e.g. without '.jpg')
-        cropToPatches(croppedImage, saveName, RESIZE_UNITS["width"], RESIZE_UNITS["height"], folderName)
-        x1 += delta + dimensionsDict["margin"] # Move the X1 axis to the next page
-        x2 += delta + dimensionsDict["margin"] # Move the X2 axis to the next page
-        imageName = os.path.splitext(imageName)[0] + "2" + os.path.splitext(imageName)[1] # give the second page a different save name
-    global NUM_OF_CROPPED_IMAGES
-    NUM_OF_CROPPED_IMAGES += 1
+    print(os.path.join(INPUT_PATH, folderName, imageName))
+    img = cv2.imread(os.path.join(INPUT_PATH, folderName, imageName),
+                     0)  # Read the image from the folder with grayscale mode
+    originalName = imageName  # For the log
+    dims = img.shape
+    h, w = dims[0], dims[1]
+    saveName = os.path.splitext(imageName)[0]
+    newImg = RGBtoBW(img)
+    cropToPatches(newImg, saveName, w, h, folderName)
     logging.info("[" + inspect.stack()[0][3] + "] - " + "Image " + originalName + " Cropped successfully.")
 
 
-def cropFiles(imagesInput, dimensionsDict: dict, folderName: str):
+def RGBtoBW(img):
+    _, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    cv2.imwrite("test.jpg", thresh)
+    return thresh
+
+
+def cropFiles(imagesInput, folderName: str):
     """
     This function calls cropSinglePage for each image in the imagesInput list, with its dimensions and input folder name.
 
@@ -101,11 +88,11 @@ def cropFiles(imagesInput, dimensionsDict: dict, folderName: str):
     folderName (str): Input folder of the images.
     """
     for imageName in imagesInput:
-        cropSinglePage(imageName, dimensionsDict, folderName)
+        cropSinglePage(imageName, folderName)
 
 
 def listToChunks(l, n):
-    """
+    """fu ck u em ilia
     This function divides the images into chunks so that the threads could work on each chunk.
 
     Prameters:
@@ -119,7 +106,7 @@ def listToChunks(l, n):
         yield l[i:i + n]
 
 
-def runThreads(folderName: str, dimensionsDict: dict):
+def runThreads(folderName: str):
     """
     This function creates output folders and runs threads on each chunk of images.
 
@@ -129,25 +116,26 @@ def runThreads(folderName: str, dimensionsDict: dict):
     """
     path = os.path.join(INPUT_PATH, folderName)
     try:
-        imagesInput = os.listdir(path) # Get all of the images from the current folder
+        imagesInput = os.listdir(path)  # Get all of the images from the current folder
     except FileNotFoundError:
         logging.error("[" + inspect.stack()[0][3] + "] - Input file '" + path + "' not found.")
         return
-    if not os.path.exists(OUTPUT_PATH): 
-        os.mkdir(OUTPUT_PATH) # Create output folder
-    if not os.path.exists(os.path.join(OUTPUT_PATH, folderName)): 
-        os.mkdir(os.path.join(OUTPUT_PATH, folderName)) # Create a folder inside output folder
+    if not os.path.exists(OUTPUT_PATH):
+        os.mkdir(OUTPUT_PATH)  # Create output folder
+    if not os.path.exists(os.path.join(OUTPUT_PATH, folderName)):
+        os.mkdir(os.path.join(OUTPUT_PATH, folderName))  # Create a folder inside output folder
     global NUM_OF_THREADS
-    if NUM_OF_THREADS == 1: # Prevent division by zero
+    if NUM_OF_THREADS == 1:  # Prevent division by zero
         NUM_OF_THREADS += 1
     i = round(len(imagesInput) // (NUM_OF_THREADS - 1))
-    chunks = list(listToChunks(imagesInput, i)) # Divide the images into chunks for the threads
-    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_OF_THREADS) as executor: # A thread pool for the threads
+    chunks = list(listToChunks(imagesInput, i))  # Divide the images into chunks for the threads
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_OF_THREADS) as executor:  # A thread pool for the threads
         for i in range(NUM_OF_THREADS):
             try:
-                executor.submit(cropFiles, chunks[i], dimensionsDict, folderName) # Add thread to the pool with its chunk
+                executor.submit(cropFiles, chunks[i], folderName)  # Add thread to the pool with its chunk
             except IndexError:
                 continue
+
 
 def cropToPatches(image, imageName: str, imageWidth: int, imageHeight: int, folderName: str):
     """
@@ -161,44 +149,109 @@ def cropToPatches(image, imageName: str, imageWidth: int, imageHeight: int, fold
     folderName (str): The name of the output folder.
     """
     x1 = y1 = 0
-    x2, y2 = PATCH_DIMENSIONS["x"], PATCH_DIMENSIONS["y"] # The dimension of the patch (current: 300X200)
-    xOffset, yOffset = PATCH_DIMENSIONS["xOffset"], PATCH_DIMENSIONS["yOffset"] # The offset of the patch (defined as 1/3 the size of the patch)
-    i = 1 # Index for naming each patch
-    while x2 < imageWidth: # End of pixels row
-        j = 0 # Index for Y axis offset
-        while y2 + yOffset * j < imageHeight: # End of pixels col
-            croppedPatch = image[y1 + yOffset * j: y2 + yOffset * j, x1: x2] # Extract the pixels of the selected patch
-            saveLocation = os.path.join(OUTPUT_PATH, folderName, imageName + "_" + str(i) + ".jpg")
+    x2, y2 = PATCH_DIMENSIONS["x"], PATCH_DIMENSIONS["y"]  # The dimension of the patch (current: 300X200)
+    xOffset, yOffset = PATCH_DIMENSIONS["xOffset"], PATCH_DIMENSIONS[
+        "yOffset"]  # The offset of the patch (defined as 1/3 the size of the patch)
+    i = 1  # Index for naming each patch
+    while x2 < imageWidth:  # End of pixels row
+        j = 0  # Index for Y axis offset
+        while y2 + yOffset * j < imageHeight:  # End of pixels col
+            croppedPatch = image[y1 + yOffset * j: y2 + yOffset * j, x1: x2]  # Extract the pixels of the selected patch
+            saveLocation = os.path.join(UNFILTERED_PATCHES_PATH, folderName, imageName + str(i) + ".jpg")
             global NUM_OF_PATCHES
-            NUM_OF_PATCHES += 1 # Patches counter
-            cv2.imwrite(saveLocation, croppedPatch) # Save the patch to the output folder
-            i += 1 
+            NUM_OF_PATCHES += 1  # Patches counter
+            if countPixels(croppedPatch):
+                plt.imsave(saveLocation, croppedPatch)  # Save the patch to the output folder
+            i += 1
             j += 1
         x1 += xOffset
         x2 += xOffset
+
+
+def countPixels(img):
+    n = PATCH_DIMENSIONS['x'] // 2
+    h, w = img.shape
+    splitted_img = []
+    k = 0
+    cv2.imshow("aa", img)
+    cv2.waitKey(0)
+    for y in range(0, h, n):
+        for x in range(0, w, n):
+            y1 = y + n
+            x1 = x + n
+            splitted_img.insert(k, img[y: y1, x:x1])
+            cv2.imshow("aa", splitted_img[k])
+            cv2.waitKey(0)
+            k += 1
+
+    summ = []
+    for i in range(4):
+        darkPixels = n * n - cv2.countNonZero(splitted_img[i])
+        summ.insert(i, darkPixels / (n ** 2))
+
+    return True
 
 
 def preProcessingMain():
     """
     This function loads the folder names from the input folder and executes the crop algorithm on each name, with its crop dimestions.
     """
+    folders_names = []
     try:
-        foldersName = os.listdir(INPUT_PATH) # Folders name from input folder (e.g. "A1", "B1"...)
+        # Folders name from input folder (e.g. "AshkenaziCursive", "BizantyCursive"...)
+        folders_names.insert(0, CURSIVE_INPUT_PATH)
+        folders_names.insert(1, SQUARE_INPUT_PATH)
+        folders_names.insert(2, SEMI_SQUARE_INPUT_PATH)
     except FileNotFoundError:
         logging.error("[" + inspect.stack()[0][3] + "] - Input file '" + INPUT_PATH + "' not found.")
-        return # The script can't run without input
+        return  # The script can't run without input
     logging.info("[" + inspect.stack()[0][3] + "] - " + "Deleting output folder")
-    subprocess.call("rm -rf " + OUTPUT_PATH + os.sep + "*", shell=True) # Delete output folder
+    # subprocess.call("rm -rf " + OUTPUT_PATH + os.sep + "*", shell=True) # Delete output folder
     logging.info("[" + inspect.stack()[0][3] + "] - " + "Done")
     for name in foldersName:
         logging.info("[" + inspect.stack()[0][3] + "] - " + "Start cropping the folder " + name + ".")
         try:
-            runThreads(name, CROP_DIMENSIONS[name]) # Pass the folder name and its dimensions
+            runThreads(name)  # Pass the folder name and its dimensions
             logging.info("[" + inspect.stack()[0][3] + "] - " + "Cropping of " + name + " succeeded.")
         except KeyError:
-            logging.error("[" + inspect.stack()[0][3] + "] - File name doesn't match to the dimensions dictionary's key.") # In case the folder name is incorrect,
-                                                                                         # or the dimensions dict doesn't contain the folder dimensions
+            logging.error("[" + inspect.stack()[0][
+                3] + "] - File name doesn't match to the dimensions dictionary's key.")  # In case the folder name is incorrect,
+            # or the dimensions dict doesn't contain the folder dimensions
             logging.error("[" + inspect.stack()[0][3] + "] - Cropping of " + name + " Failed.")
+
+
+def checkIfFolderExists(input_path):
+    if os.path.exists(input_path):
+        return True
+    return False
+
+
+def createFolders():
+    # CREATE INPUT PATHS
+    if not checkIfFolderExists(CURSIVE_INPUT_PATH):
+        os.makedirs(CURSIVE_INPUT_PATH)
+    if not checkIfFolderExists(SQUARE_INPUT_PATH):
+        os.makedirs(SQUARE_INPUT_PATH)
+    if not checkIfFolderExists(SEMI_SQUARE_INPUT_PATH):
+        os.makedirs(SEMI_SQUARE_INPUT_PATH)
+
+    # CREATE UNFILTERED PATCHES PATHS
+    if not checkIfFolderExists(CURSIVE_UNFILTERED_PATH):
+        os.makedirs(CURSIVE_UNFILTERED_PATH)
+    if not checkIfFolderExists(SQUARE_UNFILTERED_PATH):
+        os.makedirs(SQUARE_UNFILTERED_PATH)
+    if not checkIfFolderExists(SEMI_SQUARE_UNFILTERED_PATH):
+        os.makedirs(SEMI_SQUARE_UNFILTERED_PATH)
+
+    # CREATE OUTPUT PATHS
+    if not checkIfFolderExists(CURSIVE_OUTPUT_PATH):
+        os.makedirs(CURSIVE_OUTPUT_PATH)
+    if not checkIfFolderExists(SQUARE_OUTPUT_PATH):
+        os.makedirs(SQUARE_OUTPUT_PATH)
+    if not checkIfFolderExists(SEMI_SQUARE_OUTPUT_PATH):
+        os.makedirs(SEMI_SQUARE_OUTPUT_PATH)
+
+
 
 
 def main():
@@ -207,9 +260,13 @@ def main():
     """
     startTime = datetime.now()
     logging.info("[" + inspect.stack()[0][3] + "] - " + "Crop Script started")
-    preProcessingMain()
-    logging.info("[" + inspect.stack()[0][3] + "] - " + "Crop Script ended, execution time: " + str(datetime.now() - startTime))
-    logging.info("[" + inspect.stack()[0][3] + "] - " + str(NUM_OF_CROPPED_IMAGES) + " Images have been cropped into " + str(NUM_OF_PATCHES) + " Patches.")
+    createFolders()
+    # preProcessingMain()
+    logging.info(
+        "[" + inspect.stack()[0][3] + "] - " + "Crop Script ended, execution time: " + str(datetime.now() - startTime))
+    logging.info(
+        "[" + inspect.stack()[0][3] + "] - " + str(NUM_OF_CROPPED_IMAGES) + " Images have been cropped into " + str(
+            NUM_OF_PATCHES) + " Patches.")
 
 
 if __name__ == "__main__":
--- network.py
+++ network.py
@@ -31,100 +31,106 @@ session = InteractiveSession(config=config)
 
 # Get data after the PreProcessing
 def loadPatchesFromPath(path: str):
-        """
-        This function loads patches from a given path, and gives labels (boolean of Ashkenazi or not) to the patches from the same path.
+    """
+    This function loads patches from a given path, and gives labels (boolean of Ashkenazi or not) to the patches from the same path.
 
-        Parameters:
-        path (str): Path to folder with patches.
+    Parameters:
+    path (str): Path to folder with patches.
 
-        Returns:
-        list: The loaded patches with the labels.
-        """
-        dataset = []
-        ashkenazi = True # Default value for label
-        patchesCount = 0 # For logging
-        if not path[path.rfind(os.path.sep) + 1:].startswith('A'): # Starts with 'A' means Ashkenazi script
-                ashkenazi = False
-        try:
-                patchesNames = os.listdir(path)
-                patchesNum = len(patchesNames)
-        except FileNotFoundError:
-                crop.logging.error("[" + inspect.stack()[0][3] + "] - Output file '" + path + "' not found.")
-                return 
-        for patch in patchesNames:
-                patchesCount += 1
-                dataset.append(tuple((cv2.imread(os.path.join(path, patch), cv2.IMREAD_GRAYSCALE), ashkenazi))) # Append a tuple of a single patch with its label
-                if patchesCount % 10000 == 0: # For the logger
-                    crop.logging.info("[" + inspect.stack()[0][3] + "] - Loaded " + str(patchesCount) + "/" + str(patchesNum) + " Patches from " + os.path.basename(path) + ".")
-        return dataset
+    Returns:
+    list: The loaded patches with the labels.
+    """
+    dataset = []
+    ashkenazi = True  # Default value for label
+    patchesCount = 0  # For logging
+    if not path[path.rfind(os.path.sep) + 1:].startswith('A'):  # Starts with 'A' means Ashkenazi script
+        ashkenazi = False
+    try:
+        patchesNames = os.listdir(path)
+        patchesNum = len(patchesNames)
+    except FileNotFoundError:
+        crop.logging.error("[" + inspect.stack()[0][3] + "] - Output file '" + path + "' not found.")
+        return
+    for patch in patchesNames:
+        patchesCount += 1
+        dataset.append(tuple((cv2.imread(os.path.join(path, patch), cv2.IMREAD_GRAYSCALE),
+                              ashkenazi)))  # Append a tuple of a single patch with its label
+        if patchesCount % 10000 == 0:  # For the logger
+            crop.logging.info("[" + inspect.stack()[0][3] + "] - Loaded " + str(patchesCount) + "/" + str(
+                patchesNum) + " Patches from " + os.path.basename(path) + ".")
+    return dataset
+
 
 def shuffleDataset(dataset: list):
-        """
-        This function shuffles the dataset.
+    """
+    This function shuffles the dataset.
 
-        Parameters:
-        dataset (list): The dataset.
+    Parameters:
+    dataset (list): The dataset.
 
-        Returns:
-        list: The shuffled dataset.
-        """
-        random.shuffle(dataset)
-        return dataset
+    Returns:
+    list: The shuffled dataset.
+    """
+    random.shuffle(dataset)
+    return dataset
+
 
 def splitDataset(dataset: list):
-        """
-        This function splits the dataset into data and classes (labels).
+    """
+    This function splits the dataset into data and classes (labels).
 
-        Parameters:
-        dataset (list): The dataset.
+    Parameters:
+    dataset (list): The dataset.
 
-        Returns:
-        tuple: A tuple where tuple[0] is a list of the data and tuple[1] is the classes of the data.
-        """
-        data, classes = zip(*dataset)
-        return list(data), list(classes)
-
+    Returns:
+    tuple: A tuple where tuple[0] is a list of the data and tuple[1] is the classes of the data.
+    """
+    data, classes = zip(*dataset)
+    return list(data), list(classes)
 
 
 def buildData(runCrop=False):
-        """
-        This function builds the data from the output folders.
+    """
+    This function builds the data from the output folders.
 
-        Parameters:
-        runCrop (bool): Cache flag.
+    Parameters:
+    runCrop (bool): Cache flag.
 
-        Returns:
-        tuple: a tuple where tuple[0] is a list of the data and tuple[1] is the classes of the data.
-        """
-        startTime = datetime.now()
-        crop.logging.info("[" + inspect.stack()[0][3] + "] - Starting to build the data for the Neural Network.")
-        if runCrop:
-                crop.main() # PreProcessing run
-        try:
-                outputFolders = os.listdir(crop.outputFolder)
-        except FileNotFoundError:
-                crop.logging.error("[" + inspect.stack()[0][3] + "] - Output file '" + str(crop.outputFolder) + "' not found.")
-                exit(1)
+    Returns:
+    tuple: a tuple where tuple[0] is a list of the data and tuple[1] is the classes of the data.
+    """
+    startTime = datetime.now()
+    crop.logging.info("[" + inspect.stack()[0][3] + "] - Starting to build the data for the Neural Network.")
+    if runCrop:
+        crop.main()  # PreProcessing run
+    try:
+        outputFolders = os.listdir(crop.outputFolder)
+    except FileNotFoundError:
+        crop.logging.error("[" + inspect.stack()[0][3] + "] - Output file '" + str(crop.outputFolder) + "' not found.")
+        exit(1)
 
-        dataset = []
-        for name in outputFolders:
-                crop.logging.info("[" + inspect.stack()[0][3] + "] - Loading patches from " + name + " Folder.")
-                dataset += loadPatchesFromPath(os.path.join(crop.outputFolder, name)) # Append the patches list from each output folder
-                crop.logging.info("[" + inspect.stack()[0][3] + "] - Finished loading from " + name + " Folder.")
-        # Dataset is X, classes (labels) are Y
-        dataset, classes = splitDataset(shuffleDataset(dataset))
-        crop.logging.info("[" + inspect.stack()[0][3] + "] - Data build ended, execution time: " + str(datetime.now() - startTime))
-        return dataset, classes
+    dataset = []
+    for name in outputFolders:
+        crop.logging.info("[" + inspect.stack()[0][3] + "] - Loading patches from " + name + " Folder.")
+        dataset += loadPatchesFromPath(
+            os.path.join(crop.outputFolder, name))  # Append the patches list from each output folder
+        crop.logging.info("[" + inspect.stack()[0][3] + "] - Finished loading from " + name + " Folder.")
+    # Dataset is X, classes (labels) are Y
+    dataset, classes = splitDataset(shuffleDataset(dataset))
+    crop.logging.info(
+        "[" + inspect.stack()[0][3] + "] - Data build ended, execution time: " + str(datetime.now() - startTime))
+    return dataset, classes
+
 
 # Cache flag from command line
 try:
-        runCrop = False
-        if sys.argv[1] == "True" or sys.argv[1] == "true":
-                runCrop = True
+    runCrop = False
+    if sys.argv[1] == "True" or sys.argv[1] == "true":
+        runCrop = True
 except IndexError:
-        pass
+    pass
 
-df1, y1 = buildData(runCrop) # True = Starting crop process
+df1, y1 = buildData(runCrop)  # True = Starting crop process
 crop.logging.info("Calling Garbage Collector")
 gc.collect()
 crop.logging.info("Done")
@@ -145,7 +151,7 @@ gc.collect()
 crop.logging.info("Done")
 crop.logging.info("Splitting data into train and test")
 X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=testPercent, random_state=42)
-inputShape = (df.shape[1],df.shape[2], df.shape[3])
+inputShape = (df.shape[1], df.shape[2], df.shape[3])
 crop.logging.info("Calling Garbage Collector")
 del y
 del df
@@ -157,41 +163,40 @@ model = Sequential()
 
 # Add model layers
 crop.logging.info("Adding model layers")
-model.add(Conv2D(64,(3,3), activation="sigmoid", input_shape=inputShape ))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(64, (3, 3), activation="sigmoid", input_shape=inputShape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32,(3,3), activation="sigmoid", input_shape=inputShape))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(32, (3, 3), activation="sigmoid", input_shape=inputShape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32,(3,3), activation="sigmoid", input_shape=inputShape))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(32, (3, 3), activation="sigmoid", input_shape=inputShape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64,(3,3), activation="relu", input_shape=inputShape))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(64, (3, 3), activation="relu", input_shape=inputShape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 
-model.add(Dense(units = 128, activation = 'sigmoid'))
-model.add(Dense(units = 128, activation = 'sigmoid'))
-model.add(Dense(units = 64, activation = 'relu'))
-model.add(Dense(units = 32, activation = 'sigmoid'))
-model.add(Dense(units = 2, activation="softmax"))
+model.add(Dense(units=128, activation='sigmoid'))
+model.add(Dense(units=128, activation='sigmoid'))
+model.add(Dense(units=64, activation='relu'))
+model.add(Dense(units=32, activation='sigmoid'))
+model.add(Dense(units=2, activation="softmax"))
 
 # Save the best model
 crop.logging.info("Creating checkpoint")
 checkpoint = ModelCheckpoint('test1.h5', monitor='val_acc', verbose=1, save_best_only=True,
-                                   save_weights_only=True, mode='auto', period=1)
+                             save_weights_only=True, mode='auto', period=1)
 
-logDir="logs/fit/" + datetime.now().strftime("%d-%m_%H:%M:%S")
-tensorboard = TensorBoard(log_dir=logDir, histogram_freq=1,write_graph=True, write_images=True)
-
+logDir = "logs/fit/" + datetime.now().strftime("%d-%m_%H:%M:%S")
+tensorboard = TensorBoard(log_dir=logDir, histogram_freq=1, write_graph=True, write_images=True)
 
 adam = optimizers.Adam(lr=0.0001)
 
 crop.logging.info("Compiling model")
-model.compile( loss = "binary_crossentropy",
-               optimizer = adam,
-               metrics=['accuracy']
-             )
+model.compile(loss="binary_crossentropy",
+              optimizer=adam,
+              metrics=['accuracy']
+              )
 
 # Fit arguments
 crop.logging.info("Fitting arguments:")
@@ -212,14 +217,14 @@ model.summary(print_fn=crop.logging.info)
 crop.logging.info("Running the model")
 batchSize = 128
 model.fit(training_set,
-		steps_per_epoch = len(X_train)//batchSize,
-		epochs = 16,
-		validation_data = test_set,
-		validation_steps = 2000)
+          steps_per_epoch=len(X_train) // batchSize,
+          epochs=16,
+          validation_data=test_set,
+          validation_steps=2000)
 crop.logging.info("Done")
 crop.logging.info("Running validation")
-model.fit(X_train, y_train, validation_data=(X_test, y_test),batch_size=batchSize, validation_split=0.2,
-                               epochs=16, verbose=2, callbacks=[checkpoint, tensorboard] )
+model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=batchSize, validation_split=0.2,
+          epochs=16, verbose=2, callbacks=[checkpoint, tensorboard])
 scores = model.evaluate(X_test, y_test, verbose=1)
 crop.logging.info("Done")
 crop.logging.info("Test accuracy: " + str(scores[1] * 100))
