56d8da820098523c3b5e5620d62a4c2f014bbb15
Aviel Cohen <38301471+AvielCo@users.noreply.github.com>
2020-11-24 09:11:58 +0200

    24/11: added log

---
+++ dual_print.py
@@ -0,0 +1,10 @@
+import logging as log
+
+
+def dual_print(massage, level="info"):
+    if level == "info":
+        log.info(massage)
+        print(massage)
+    elif level == "error":
+        log.error(massage)
+        print(massage)
