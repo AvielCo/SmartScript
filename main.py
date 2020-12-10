c7485eadb42a9b43b2d645b7bf6a49b235366671
aviel <Avielcohen15@gmail.com>
2020-12-10 18:02:37 +0200

    added original VGG19
    Signed-off-by: aviel <Avielcohen15@gmail.com>

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ try:
     if args.train:
         model = str(args.train[0]).lower()
         if not models.__contains__(model):
-            raise NameError("model must contain one of: " + models)
+            raise NameError(f"model must contain one of: {models}")
         times = int(args.train[1])
         if times <= 0:
             raise ValueError
@@ -57,13 +57,13 @@ try:
     elif args.test:
         model = str(args.test[0]).lower()
         if not models.__contains__(model):
-            raise NameError("model must contain one of: " + models)
+            raise NameError(f"model must contain one of: {models}")
         test_model(model)
 
     elif args.predict:
         model = str(args.predict[0]).lower()
         if not models.__contains__(model):
-            raise NameError("model must contain one of: " + models)
+            raise NameError(f"model must contain one of: {models}")
         pass  # TODO: add predict to each model
 
     elif args.crop:
