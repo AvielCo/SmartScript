57b5a5c53889db5393cb9ccdf3592a7507a96ba8
Emilia <emiliazorin@gmail.com>
2021-03-02 13:20:43 +0200

    design Scan Buttons done.
    Signed-off-by: Emilia <emiliazorin@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
+import "./App.css";
+import InputButton from "./components/Buttons/InputButton";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="Scan">
+      <InputButton name="Upload Image"></InputButton>
+      <InputButton name="Scan Selected Image"></InputButton>
+    </div>
+  );
 }
-
 export default App;
