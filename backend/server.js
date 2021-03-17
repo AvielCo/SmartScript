6bf7186ec028e407e05fcf3966deb02d60dfdf28
Emilia Zorin <39198248+EmiliaZorin@users.noreply.github.com>
2021-03-17 12:57:09 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const morgan = require('morgan');
 const createError = require('http-errors');
 const { verifyAccessToken } = require('./helpers/jwt');
-require('./helpers/redis');
 require('dotenv').config();
 require('./helpers/mongodb');
 
