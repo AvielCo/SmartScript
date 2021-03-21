f411b45d4a9e2ab0093efda84cd483cfcc137e57
AvielCo <avielcohen15@gmail.com>
2021-03-21 13:42:42 +0200

    [BACKEND] Upload image and save in DB Fixes #49 (changed upload.js)

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { verifyAccessToken } = require('../helpers/jwt');
+const User = require('../models/User');
 const router = express.Router();
 
-router.post('/upload', (req, res) => {
-  res.send('upload');
+router.post('/', verifyAccessToken, async (req, res, next) => {
+  console.log(req.headers);
+  const user = await User.findById(req.payload.aud);
+  res.json(user);
 });
 
 module.exports = router;
