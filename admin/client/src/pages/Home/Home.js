a70a48176074b37bb37c479daaf94556e57b6125
Emilia <emiliazorin@gmail.com>
2021-04-03 18:01:57 +0300

    [ADMIN] create Home page with actions Fixes #77
    Signed-off-by: Emilia <emiliazorin@gmail.com>

--- admin/client/src/pages/Home/Home.js
+++ admin/client/src/pages/Home/Home.js
@@ -1,11 +1,79 @@
-import React from 'react'
+import React from 'react';
+import "antd/dist/antd.css";
+import "./Home.css";
+import { Table, Space } from "antd";
+import { InputButton } from "../../components/Buttons";
+import axios from 'axios';
 
-function Home() {
-    return (
-        <div>
-            
+function Home (){
+
+const columns = [
+    {
+        title: "Name",
+        dataIndex: "name",
+        key: "name",
+    },
+    {
+        title: "Title",
+        dataIndex: "title",
+        key: "title",
+    },
+    {
+        title: "Actions",
+        key: "actions",
+        render: (text, record) => (
+            <Space size="middle">
+            <a>Block</a>
+            <a>Go To Profile</a>
+            </Space>
+        ),
+    },
+];
+
+// componentDidMount=() => {
+//     this.fetchData();
+// }
+
+// fetchData = () =>{
+//     axios.get().
+//     than((response) =>{
+//         const data = response.data;
+//         this.setState(something here);
+//         console.log("data had been received");
+//     })
+//     .catch(()=>{
+//         alert("error retrieving data!!!")
+//     });
+// }
+
+const data = [
+  {
+    key: "1",
+    name: "Aviel Cohen",
+    title: "engineer"
+  },
+  {
+    key: "2",
+    name: "Noah Solomon",
+    title: "engineer"
+  },
+  {
+    key: "3",
+    name: "Emilia Zorin",
+    title: "engineer"
+  },
+];
+
+return (
+    <div className="home-holder">
+        <div className="users-table-container">
+            <Table columns={columns} dataSource={data} />
         </div>
+        <div className="logout-btn">
+            <InputButton name="Logout"></InputButton>
+        </div>
+    </div>
     )
 }
 
-export default Home
+export default Home;
