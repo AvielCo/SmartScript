b5ce94810ae2a26bff9c9864196f1aa5599a03ab
AvielCo <avielcohen15@gmail.com>
2021-04-03 18:54:46 +0300

    added some actions and changed User model

--- admin/backend/routes/actions.js
+++ admin/backend/routes/actions.js
@@ -1,5 +1,64 @@
 const express = require('express');
 const router = express.Router();
-const Admin = require('../../../models/Admin');
+const User = require('../../../models/User');
 const createError = require('http-errors');
-const { verifyRefreshToken, verifyAccessToken } = require('../../../helpers/jwt');
+const { verifyAccessToken } = require('../../../helpers/jwt');
+
+router.get('/get-all-users', verifyAccessToken, async (req, res, next) => {
+  try {
+    const users = await User.find();
+    if (users.length <= 0) {
+      return res.status(204);
+    }
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/ban-user', verifyAccessToken, async (req, res, next) => {
+  try {
+    if (!req.body.userId) {
+      throw createError.BadRequest();
+    }
+    const user = await User.findByIdAndUpdate({ _id: req.body.userId }, { banned: true });
+    if (!user) {
+      return res.status(204).send('User not found');
+    }
+    return res.status(200).json('User banned');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/unban-user', verifyAccessToken, async (req, res, next) => {
+  try {
+    if (!req.body.userId) {
+      throw createError.BadRequest();
+    }
+    const user = await User.findByIdAndUpdate({ _id: req.body.userId }, { banned: false });
+    if (!user) {
+      return res.status(204).send('User not found');
+    }
+    return res.status(200).json('User unbanned');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/user-profile', verifyAccessToken, async (req, res, next) => {
+  try {
+    if (!req.body.userId) {
+      throw createError.BadRequest();
+    }
+    const user = await User.findById({ _id: req.body.userId });
+    if (!user) {
+      return res.status(204).send('User not found');
+    }
+    return res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
