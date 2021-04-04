ad80cabd358316221a566d08d3db5a5db73271ee
Emilia <emiliazorin@gmail.com>
2021-04-04 12:44:23 +0300

    [ADMIN] create Home page with actions Fixes #77
    Signed-off-by: Emilia <emiliazorin@gmail.com>

--- admin/client/src/pages/Home/Home.js
+++ admin/client/src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "antd/dist/antd.css";
 import "./Home.css";
 import { Table, Space } from "antd";
@@ -7,73 +7,61 @@ import axios from 'axios';
 
 function Home (){
 
-const columns = [
-    {
-        title: "Name",
-        dataIndex: "name",
-        key: "name",
-    },
-    {
-        title: "Title",
-        dataIndex: "title",
-        key: "title",
-    },
-    {
-        title: "Actions",
-        key: "actions",
-        render: (text, record) => (
-            <Space size="middle">
-            <a>Block</a>
-            <a>Go To Profile</a>
-            </Space>
-        ),
-    },
-];
+    const [data,setData] = useState([{}]);
 
-// componentDidMount=() => {
-//     this.fetchData();
-// }
+    const columns = [
+        {
+            title: "Name",
+            dataIndex: "name",
+            key: "name",
+        },
+        {
+            title: "Banned",
+            dataIndex: " banned",
+            key: "banned",
+        },
+        {
+            title: "Actions",
+            key: "actions",
+            render: (text, record) => (
+                <Space size="middle">
+                <a>Block</a>
+                <a>Go To Profile</a>
+                </Space>
+            ),
+        },
+    ];
 
-// fetchData = () =>{
-//     axios.get().
-//     than((response) =>{
-//         const data = response.data;
-//         this.setState(something here);
-//         console.log("data had been received");
-//     })
-//     .catch(()=>{
-//         alert("error retrieving data!!!")
-//     });
-// }
 
-const data = [
-  {
-    key: "1",
-    name: "Aviel Cohen",
-    title: "engineer"
-  },
-  {
-    key: "2",
-    name: "Noah Solomon",
-    title: "engineer"
-  },
-  {
-    key: "3",
-    name: "Emilia Zorin",
-    title: "engineer"
-  },
-];
+    useEffect(() =>{
+        axios.get("http://localhost:8080/api/actions/get-all-users")
+        .then((res)=>{
+                const users = res.data;
+                let i = 0; 
+                users.forEach((user) =>{
+                    user={...user,'key':i}
+                    i++
+                    console.log(user)
+                })
+                setData(users);
 
-return (
-    <div className="home-holder">
-        <div className="users-table-container">
-            <Table columns={columns} dataSource={data} />
+            }
+        ).catch((err)=>{
+            console.log(err)})
+    },[])
+
+
+
+    return (
+        <div className="home-holder">
+            <div className="users-table-container">
+                <Table columns={columns} dataSource={data} />
+            </div>
+            <div className="logout-btn">
+                <InputButton name="Logout"></InputButton>
+            </div>
         </div>
-        <div className="logout-btn">
-            <InputButton name="Logout"></InputButton>
-        </div>
-    </div>
-    )
-}
+        )
+    }
 
 export default Home;
