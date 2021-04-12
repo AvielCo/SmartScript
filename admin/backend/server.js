7ed6cfe47acb8a8c129b27f2c82156f647be1463
AvielCo <avielcohen15@gmail.com>
2021-04-12 10:51:46 +0300

    changes static build dir to fit the current dir

--- admin/backend/server.js
+++ admin/backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const morgan = require('morgan');
 const createError = require('http-errors');
 require('dotenv').config();
@@ -13,7 +14,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', express.static('./client/build'));
+app.use('/', express.static(path.join(__dirname, 'build')));
 
 //* Routes
 
--- user/backend/server.js
+++ user/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
 const createError = require('http-errors');
 require('dotenv').config();
 require('../../helpers/mongodb');
@@ -13,7 +14,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', express.static('./client/build'));
+app.use('/', express.static(path.join(__dirname, 'build')));
 
 //* Routes
 
