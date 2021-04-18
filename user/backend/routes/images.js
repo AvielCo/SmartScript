3b8507a28ff6e1e6de7a7091ab5e2efbd77707b8
AvielCo <avielcohen15@gmail.com>
2021-04-18 16:07:14 +0300

    fixed predict in router

--- user/backend/routes/images.js
+++ user/backend/routes/images.js
@@ -52,89 +52,69 @@ router.post('/scan', verifyAccessToken, async (req, res, next) => {
     const pythonScriptCommand = `python ${pythonScriptPath}  ${user._id}`;
     const envName = 'py36';
     const condaCommand = `conda run -n ${envName}`;
-    // const child = exec(`${condaCommand} ${pythonScriptCommand}`);
-    const imagePath = path.join(__dirname, '..', 'python-folders', 'predict-files', 'predict_images', `${user._id}`, 'imageToUpload.jpg');
-    const userHistory = await History.findById({ _id: user.historyId });
-    // path to save the resized image to view later in the user profile
-    const savePath = path.join(__dirname, '..', 'users-histories', `${user._id}`);
-    fs.mkdir(savePath, { recursive: true }, (err) => {
-      if (err) throw createError.InternalServerError();
+    const child = exec(`${condaCommand} ${pythonScriptCommand}`);
+
+    child.stdout.on('data', async (data) => {
+      // message is the response from python script
+      const message = JSON.parse(data);
+      if (message.success) {
+        /**
+         * message: {
+         *  success: True,
+         *  origin: One of the following: "ashkenazi", "bizantine" .....
+         *  shape: One of the following: "cursive", "square", "semi-square"
+         *  probability: the probability of the prediction
+         * }
+         */
+        try {
+          let totalImages = 0; // total images that the user has seen predicted
+          const imagePath = path.join(__dirname, '..', 'python-folders', 'predict-files', 'predict_images', `${user._id}`, 'imageToUpload.jpg');
+          const userHistory = await History.findById({ _id: user.historyId });
+          if (userHistory.predictedResult.classes.length > 0) {
+            // get the amount of the images that the user has predicted so far.
+            totalImages = userHistory.predictedResult.classes.length;
+          }
+          // path to save the resized image to view later in the user profile
+          const savePath = path.join(__dirname, '..', 'users-histories', `${user._id}`);
+          fs.mkdir(savePath, { recursive: true }, (err) => {
+            if (err) throw createError.InternalServerError();
+          });
+
+          // generate unique hash for image name
+          const imageName = genRandomString(40);
+
+          sharp(imagePath) // resize the image to width: 250px (height is auto scale)
+            .resize(250)
+            .toFile(path.join(savePath, `${imageName}.jpg`))
+            .catch((err) => {
+              if (err) throw createError.InternalServerError();
+            });
+
+          await insertNewHistory(userHistory, message, imageName);
+
+          return res.status(200).send(message);
+        } catch (err) {
+          console.log(err);
+          return next(createError.InternalServerError());
+        }
+      }
+      /**
+       * message: {
+       *  success: False,
+       * `reason: Reason that the script failed
+       * }
+       */
+      console.log(message.reason);
+      return next(createError.BadRequest());
     });
 
-    // generate unique hash for image name
-    const imageName = genRandomString(40);
+    child.stderr.on('data', (data) => {
+      console.log(data);
+    });
 
-    sharp(imagePath) // resize the image to width: 250px (height is auto scale)
-      .resize(250)
-      .toFile(path.join(savePath, `${imageName}.jpg`))
-      .catch((err) => {
-        if (err) throw createError.InternalServerError();
-      });
-    const message = { success: true, origin: 'test', shape: 'test', probability: 'test' };
-    await insertNewHistory(userHistory, message, imageName);
-
-    return res.status(200).send(message);
-    //   child.stdout.on('data', async (data) => {
-    //     // message is the response from python script
-    //     const message = JSON.parse(data);
-    //     if (message.success) {
-    //       /**
-    //        * message: {
-    //        *  success: True,
-    //        *  origin: One of the following: "ashkenazi", "bizantine" .....
-    //        *  shape: One of the following: "cursive", "square", "semi-square"
-    //        *  probability: the probability of the prediction
-    //        * }
-    //        */
-    //       try {
-    //         let totalImages = 0; // total images that the user has seen predicted
-    //         const imagePath = path.join(__dirname, '..', 'python-folders', 'predict-files', 'predict_images', `${user._id}`, 'imageToUpload.jpg');
-    //         const userHistory = await History.findById({ _id: user.historyId });
-    //         if (userHistory.predictedResult.classes.length > 0) {
-    //           // get the amount of the images that the user has predicted so far.
-    //           totalImages = userHistory.predictedResult.classes.length;
-    //         }
-    //         // path to save the resized image to view later in the user profile
-    //         const savePath = path.join(__dirname, '..', 'users-histories', `${user._id}`);
-    //         fs.mkdir(savePath, { recursive: true }, (err) => {
-    //           if (err) throw createError.InternalServerError();
-    //         });
-
-    //         // generate unique hash for image name
-    //         const imageName = genRandomString(40);
-
-    //         sharp(imagePath) // resize the image to width: 250px (height is auto scale)
-    //           .resize(250)
-    //           .toFile(path.join(savePath, `${imageName}.jpg`))
-    //           .catch((err) => {
-    //             if (err) throw createError.InternalServerError();
-    //           });
-
-    //         await insertNewHistory(userHistory, message, imageName);
-
-    //         return res.status(200).send(message);
-    //       } catch (err) {
-    //         console.log(err);
-    //         return next(createError.InternalServerError());
-    //       }
-    //     }
-    //     /**
-    //      * message: {
-    //      *  success: False,
-    //      * `reason: Reason that the script failed
-    //      * }
-    //      */
-    //     console.log(message.reason);
-    //     return next(createError.BadRequest());
-    //   });
-
-    //   child.stderr.on('data', (data) => {
-    //     console.log(data);
-    //   });
-
-    //   child.on('error', function (err) {
-    //     throw next(createError.InternalServerError());
-    //   });
+    child.on('error', function (err) {
+      throw next(createError.InternalServerError());
+    });
   } catch (err) {
     console.log(err);
     next(err);
