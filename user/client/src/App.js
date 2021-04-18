91f0a2544e00a3ba52f5e35b3e1a89f696954985
Emilia <emiliazorin@gmail.com>
2021-04-18 16:03:27 +0300

    [FRONT] Design a ban page for banned users Fixes #87
    Signed-off-by: Emilia <emiliazorin@gmail.com>

--- user/client/src/App.js
+++ user/client/src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import React, { useEffect, useState } from 'react';
-import { Home, Login, Register, Profile, Error, Logout } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { getAccessToken } from './helpers';
-import 'antd/dist/antd.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { Home, Login, Register, Profile, Error, Logout, Banned } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { getAccessToken } from "./helpers";
+import "antd/dist/antd.css";
 
 function App() {
   const [token, setToken] = useState(getAccessToken());
   useEffect(() => {
-    window.addEventListener('storage', (ev) => {
+    window.addEventListener("storage", (ev) => {
       setToken(getAccessToken());
     });
     return () => {
-      window.removeEventListener('storage', (ev) => {});
+      window.removeEventListener("storage", (ev) => {});
     };
   }, []);
 
@@ -24,6 +24,7 @@ function App() {
         <Route exact path="/login" component={token ? Home : Login} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/logout" component={token ? Logout : Home} />
+        <Route exact path="/ban" component={Banned} />
         <Route exact path="/" component={Home} />
         <Route component={Error} />
       </Switch>
