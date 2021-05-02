19e084ac273968205360f837f4426c4222e6f0ff
AvielCo <avielcohen15@gmail.com>
2021-05-02 19:05:18 +0300

    Added mail service on register

--- user/backend/routes/auth.js
+++ user/backend/routes/auth.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../../../models/User');
-const History = require('../../../models/History');
-const createError = require('http-errors');
-const authSchema = require('../validations/auth');
-const { decryptStrings } = require('../../../helpers/crypto');
-const { signAccessToken, verifyAccessToken } = require('../../../helpers/jwt');
-const redis = require('../../../helpers/redis');
-require('dotenv').config();
+const User = require("../../../models/User");
+const History = require("../../../models/History");
+const createError = require("http-errors");
+const authSchema = require("../validations/auth");
+const { decryptStrings } = require("../../../helpers/crypto");
+const { signAccessToken, verifyAccessToken } = require("../../../helpers/jwt");
+const redis = require("../../../helpers/redis");
+const { sendCredentialEmail } = require("../../../helpers/nodemailer");
+require("dotenv").config();
 
-router.post('/register', async (req, res, next) => {
+router.post("/register", async (req, res, next) => {
   try {
     const { email, username, password, name } = decryptStrings({ email: req.body.email }, { username: req.body.username }, { password: req.body.password }, { name: req.body.name });
     if (!email || !username || !password || !name) {
@@ -33,10 +34,10 @@ router.post('/register', async (req, res, next) => {
       //! User is exists
       //! Check which fields are the same and throw an error
       if (userExists.username === newUserDetails.username) {
-        throw createError.Conflict('Username is already in use.');
+        throw createError.Conflict("Username is already in use.");
       }
       if (userExists.email === newUserDetails.email) {
-        throw createError.Conflict('Email is already in use.');
+        throw createError.Conflict("Email is already in use.");
       }
       throw createError.Conflict();
     }
@@ -45,6 +46,8 @@ router.post('/register', async (req, res, next) => {
     const newUser = await new User(newUserDetails).save();
     const history = await new History({ userId: newUser._id, predictedResult: { classes: [], probabilities: [], dates: [] } }).save();
     await User.findByIdAndUpdate(newUser._id, { historyId: history._id });
+
+    sendCredentialEmail(name, username, password, email);
 
     res.sendStatus(200);
   } catch (err) {
@@ -56,18 +59,18 @@ router.post('/register', async (req, res, next) => {
   }
 });
 
-router.post('/login', async (req, res, next) => {
+router.post("/login", async (req, res, next) => {
   try {
     const { username, password } = decryptStrings({ username: req.body.username }, { password: req.body.password });
-    const user = await User.findOne({ username }).select('+password');
+    const user = await User.findOne({ username }).select("+password");
 
     if (!user) {
-      throw createError.Unauthorized('Username or password are incorrect.');
+      throw createError.Unauthorized("Username or password are incorrect.");
     }
 
     const isMatch = await user.isValidPassword(password);
     if (!isMatch) {
-      throw createError.Unauthorized('Username or password are incorrect.');
+      throw createError.Unauthorized("Username or password are incorrect.");
     }
 
     const accessToken = await signAccessToken(user.id);
@@ -75,15 +78,15 @@ router.post('/login', async (req, res, next) => {
     res.status(200).json({ accessToken });
   } catch (err) {
     if (err.isJoi) {
-      return next(createError.Unauthorized('Username or password are incorrect.'));
+      return next(createError.Unauthorized("Username or password are incorrect."));
     }
     next(err);
   }
 });
 
-router.get('/user', verifyAccessToken, async (req, res, next) => {
+router.get("/user", verifyAccessToken, async (req, res, next) => {
   try {
-    const userId = req.payload['aud'];
+    const userId = req.payload["aud"];
     return res.sendStatus(200);
   } catch (err) {
     console.log(err);
@@ -91,9 +94,9 @@ router.get('/user', verifyAccessToken, async (req, res, next) => {
   }
 });
 
-router.delete('/logout', verifyAccessToken, async (req, res, next) => {
+router.delete("/logout", verifyAccessToken, async (req, res, next) => {
   try {
-    const userId = req.payload['aud'];
+    const userId = req.payload["aud"];
     redis.DEL(userId, (error, reply) => {
       if (error) {
         throw createError.InternalServerError();
