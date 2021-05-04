b7ee02245688f7287f10440228d1f81c0af52af8
SoloNoah <39374814+SoloNoah@users.noreply.github.com>
2021-05-04 12:16:43 +0300

    design changes

--- user/client/src/components/TextView/ResultTextView.js
+++ user/client/src/components/TextView/ResultTextView.js
@@ -7,16 +7,12 @@ const ResultTextView = ({ result }) => {
   return (
     <div>
       <div className='results'>
-        {result.success ? (
+        {result.success && (
           <div className='results-text-view'>
             <Paragraph className='results-text'>
               {`${result.origin}-${result.shape} `} {result.probability}%
             </Paragraph>
           </div>
-        ) : (
-          <Text ellipsis strong>
-            Upload an image and click on predict to see results!
-          </Text>
         )}
       </div>
       {result.success && <Paragraph strong>Added to your history, check your profile.</Paragraph>}
--- user/client/src/pages/Home/Home.js
+++ user/client/src/pages/Home/Home.js
@@ -1,31 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { NavBar, ResultTextView } from "../../components";
-import pic from "../../assets/landing-bg.jpg";
-import { ToastContainer, toast } from "react-toastify";
-import axios from "axios";
-import { useHistory } from "react-router-dom";
-import { getAccessToken } from "../../helpers";
-import { Upload } from "antd";
-import Card from "../../components/Card/ProfileCard";
-import Button from "../../components/Buttons/InputButton";
+import React, { useState } from 'react';
+import { NavBar, ResultTextView } from '../../components';
+import pic from '../../assets/landing-bg.jpg';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
 
-import cursive from "../../assets/cursive_trans.png";
+import { getAccessToken } from '../../helpers';
+import { Upload } from 'antd';
+import Card from '../../components/Card/ProfileCard';
+import Button from '../../components/Buttons/InputButton';
+import { HashLink as HashLink } from 'react-router-hash-link';
 
-import Emilia from "../../assets/emilia.jpg";
-import Noah from "../../assets/noah.png";
-import Aviel from "../../assets/aviel.png";
+import cursive from '../../assets/cursive_trans.png';
 
-import "react-toastify/dist/ReactToastify.css";
-import "./Home.css";
+import Emilia from '../../assets/emilia.jpg';
+import Noah from '../../assets/noah.png';
+import Aviel from '../../assets/aviel.png';
+
+import 'react-toastify/dist/ReactToastify.css';
+import './Home.css';
 
 function LandingSection() {
   return (
     <section className='landing'>
-      <div>
-        <p dir='rtl'>
-          <h2>SmartScript</h2> was developed to solve the problem in categorizing hebrew ancient scripts. This website will identify the origin and the shape of the script given to it by scanning the
-          image given to it by the user. Additionally, the system will allow the user to save previous uploads for easy tracking.
+      <div className='landing-div'>
+        <p>
+          <bold>SmartScript</bold> was developed to solve the problem in categorizing hebrew ancient scripts. This website will identify the origin and the shape of the script given to it by scanning
+          the image given to it by the user. Additionally, the system will allow the user to save previous uploads for easy tracking.
         </p>
+        <HashLink to='home#scan' smooth>
+          <Button className='move-to-scan' name={'Proceed to predict an image'}></Button>
+        </HashLink>
       </div>
     </section>
   );
@@ -36,21 +40,21 @@ function ScanSection({ isLoggedIn }) {
   const [isLoading, setIsLoading] = useState(false);
   const [result, setResult] = useState({
     success: false,
-    origin: "",
-    shape: "",
-    probability: "",
+    origin: '',
+    shape: '',
+    probability: '',
   });
 
   const handleImageChange = async (info) => {
     switch (info.file.status) {
-      case "uploading":
+      case 'uploading':
         setIsLoading(true);
         break;
-      case "done":
+      case 'done':
         setIsLoading(false);
         setImageUri(URL.createObjectURL(info.file.originFileObj));
         break;
-      case "error":
+      case 'error':
         setIsLoading(false);
         break;
       default:
@@ -60,21 +64,21 @@ function ScanSection({ isLoggedIn }) {
 
   const handlePredict = () => {
     if (!imageUri) {
-      toast.error("Upload an image before predict.");
+      toast.error('Upload an image before predict.');
       return;
     }
     if (isLoading) {
-      toast.info("Please wait, another predict process is running.");
+      toast.info('Please wait, another predict process is running.');
       return;
     }
-    toast.info("Predicting image, please wait.");
+    toast.info('Predicting image, please wait.');
     setIsLoading(true);
 
     let accessToken = getAccessToken();
 
     const cfg = {
       headers: {
-        Authorization: "Bearer " + accessToken,
+        Authorization: 'Bearer ' + accessToken,
       },
     };
 
@@ -84,41 +88,40 @@ function ScanSection({ isLoggedIn }) {
         if (res.status === 200) {
           setResult(res.data);
           setIsLoading(false);
-          toast.success("Predicting image done, see the result in your profile page.");
+          toast.success('Predicting image done, see the result in your profile page.');
         }
       })
       .catch((err) => {
         if (err) {
-          toast.error("An error has been encountered while trying to predict.");
+          toast.error('An error has been encountered while trying to predict.');
           setIsLoading(false);
         }
       });
   };
 
   return (
-    <section className="scan" id="scan">
-      <div className="scan-container">
-        {isLoggedIn ? (
-          <form className='scan-btn-holder' onSubmit={handleImageChange}>
-            <h3>Upload and Predict</h3>
-            <Upload
-              action={`${process.env.REACT_APP_API_ADDRESS}/api/images/upload`}
-              headers={{ Authorization: "Bearer " + getAccessToken() }}
-              onChange={handleImageChange}
-              accept='image/*'
-              maxCount={1}
-              showUploadList={false}>
-              <Button className="scan-btn" component="span" type="submit" name={"Upload"} disabled={isLoading} />
-            </Upload>
-            <Button className="scan-btn" onClick={handlePredict} name={"Predict"} disabled={isLoading}></Button>
-            <div className="scan-btn"></div>
-            <ResultTextView result={result} />
-          </form>
-        ) : (
+    <section className='scan' id='scan'>
+      <div className='scan-container'>
+        <form className='scan-btn-holder' onSubmit={handleImageChange}>
           <h3>
-            Please login to gain access to the "Scan" feature. <br /> Consider registering if you don't have an account.
+            <bold>Upload and Predict</bold>
           </h3>
-        )}
+          <p>Upload an image and click on predict to see results!</p>
+          <Upload
+            action={`${process.env.REACT_APP_API_ADDRESS}/api/images/upload`}
+            headers={{ Authorization: 'Bearer ' + getAccessToken() }}
+            onChange={handleImageChange}
+            accept='image/*'
+            maxCount={1}
+            showUploadList={false}
+          >
+            <Button className='scan-btn' component='span' type='submit' name={'Upload'} disabled={isLoading} />
+          </Upload>
+          <Button className='scan-btn' onClick={handlePredict} name={'Predict'} disabled={isLoading}></Button>
+          <div className='scan-btn'></div>
+          <ResultTextView result={result} />
+        </form>
+
         {imageUri && (
           <div className='scan-img-holder'>
             <img alt={pic} src={imageUri}></img>
@@ -131,10 +134,10 @@ function ScanSection({ isLoggedIn }) {
 
 function AboutSection() {
   return (
-    <section className="about" id="about">
+    <section className='about' id='about'>
       <h3>About the project</h3>
-      <div className="about-content">
-        <div className="about-text-holder">
+      <div className='about-content'>
+        <div className='about-text-holder'>
           <p>
             By uploading an image and pressing the scan button the system will scan the image and return a predicted value for that given image. The system does it by spliting the image into 2 single
             paged images if the image contains 2 pages followed by dividing the image into small resolutions patches.
@@ -155,7 +158,7 @@ function AboutSection() {
 
 function WWASection() {
   return (
-    <section className="wwa" id="wwa">
+    <section className='wwa' id='wwa'>
       <h3>Who we are</h3>
       <p>
         We are 4th year Software Engineering students at SCE Sami Shamoon. We decided to join Dr. Irina Rabaev and with her guidance tackle this challenge. During summer time and the first semester we
@@ -166,10 +169,10 @@ function WWASection() {
         front to back. Combining RESTapi to handle requests and JWT we developed a secure login and registration. Additionally, we made sure that our website will be functional and responsive on
         different devices and resolutions
       </p>
-      <div className="cards-holder">
-        <Card name={"Noah Solomon"} image={Noah} />
-        <Card name={"Emilia Zorin"} image={Emilia} />
-        <Card name={"Aviel Cohen"} image={Aviel} />
+      <div className='cards-holder'>
+        <Card name={'Noah Solomon'} image={Noah} />
+        <Card name={'Emilia Zorin'} image={Emilia} />
+        <Card name={'Aviel Cohen'} image={Aviel} />
       </div>
     </section>
   );
@@ -178,8 +181,8 @@ function WWASection() {
 function Home({ isLoggedIn, setIsLoggedIn }) {
   return (
     <>
-      <ToastContainer position="top-left" autoClose={5000} hideProgressBar={false} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
-      <div className="home" id="home">
+      <ToastContainer position='top-left' autoClose={5000} hideProgressBar={false} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+      <div className='home' id='home'>
         <NavBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
         <LandingSection />
         <ScanSection isLoggedIn={isLoggedIn} />
