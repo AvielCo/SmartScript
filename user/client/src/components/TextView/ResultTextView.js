862373b36376312b4c1e38a2e5d975c28db594bb
Aviel Cohen <38301471+AvielCo@users.noreply.github.com>
2021-05-30 18:21:17 +0300

    updated predict results (#139)

--- user/client/src/components/TextView/ResultTextView.js
+++ user/client/src/components/TextView/ResultTextView.js
@@ -4,17 +4,43 @@ import "./ResultTextView.css";
 
 const ResultTextView = ({ result, savedToHistory: { saved, reason } }) => {
   const { Paragraph } = Typography;
+
+  const top_result = result.success
+    ? {
+        shape: result.top_results_shape[0][0],
+        origin: result.top_results_origin[0][0],
+        probability: result.top_results_origin[0][1],
+      }
+    : {
+        shape: "none",
+        origin: "none",
+        probability: "none",
+      };
+  const mid_result = result.success
+    ? {
+        shape: result.top_results_shape[0][0],
+        origin: result.top_results_origin[1][0],
+        probability: result.top_results_origin[1][1],
+      }
+    : {
+        shape: "none",
+        origin: "none",
+        probability: "none",
+      };
+
   return (
     <div>
       <div className="results">
-        {result.success && (
+        {!result.success && (
           <>
             <div className="results-text-view">
-              <Paragraph className="results-text" underline>
-                Results
+              <Paragraph className="results-text result">
+                <u>Top result:</u>
+                {` ${top_result.origin}-${top_result.shape} `} {top_result.probability}%
               </Paragraph>
               <Paragraph className="results-text result">
-                {`:   ${result.origin}-${result.shape} `} {result.probability}%
+                <u>Second result:</u>
+                {` ${mid_result.origin}-${mid_result.shape} `} {mid_result.probability}%
               </Paragraph>
             </div>
             <Paragraph strong underline>
